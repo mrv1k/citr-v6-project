4dec3725cce8076710599d1e3c96db0db3179f00
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-03-16 17:36:41 -0400

    up to 08-react-router

--- 00-follow-along/src/App.js
+++ 00-follow-along/src/App.js
@@ -1,14 +1,34 @@
+import { StrictMode } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { render } from "react-dom";
-// import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
